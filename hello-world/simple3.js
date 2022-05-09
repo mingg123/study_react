f02f7d1403b8daed1f7c763e8fa622af5f991e33
minji_kim2 <minji_kim2@tmax.co.kr>
2022-05-09 15:11:48 +0900

    feat : babel example simple4.js 코드를 jsx문법으로 작성 이후 createElement함수를 사용하는 파일 형태로 변환

--- hello-world/simple3.js
+++ hello-world/simple3.js
@@ -1,8 +1,8 @@
 function LikedButton() {
   const [liked, setLiked] = React.useState(false);
-  const text = liked ? '좋아요 취소' : '좋아요';
+  const text = liked ? "좋아요 취소" : "좋아요";
   return React.createElement(
-    'button',
+    "button",
     { onClick: () => setLiked(!liked) },
     text
   );
@@ -11,40 +11,27 @@ function LikedButton() {
 function Container() {
   const [count, setCount] = React.useState(0);
   return React.createElement(
-    'div',
+    "div",
     null,
     React.createElement(LikedButton),
     React.createElement(
-      'div',
+      "div",
       { style: { marginTop: 20 } },
-      React.createElement('span', null, '현재 카운트'),
-      React.createElement('span', null, count),
+      React.createElement("span", null, "현재 카운트"),
+      React.createElement("span", null, count),
       React.createElement(
-        'button',
+        "button",
         { onClick: () => setCount(count + 1) },
-        '증가'
+        "증가"
       ),
       React.createElement(
-        'button',
+        "button",
         { onClick: () => setCount(count - 1) },
-        '감소'
+        "감소"
       )
     )
   );
 }
 
-const domContainer = document.querySelector('#react-root');
+const domContainer = document.querySelector("#react-root");
 ReactDOM.render(React.createElement(Container), domContainer);
-
-ReactDOM.render(
-  React.createElement(LikedButton),
-  document.querySelector('#react-root1')
-);
-ReactDOM.render(
-  React.createElement(LikedButton),
-  document.querySelector('#react-root2')
-);
-ReactDOM.render(
-  React.createElement(LikedButton),
-  document.querySelector('#react-root3')
-);
