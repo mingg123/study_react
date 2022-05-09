5630abdff324a5796533c517ea45b591d493a1d1
minji_kim2 <minji_kim2@tmax.co.kr>
2022-05-09 16:56:18 +0900

    feat : add css example class이름 중첩으로 인해 style이 제대로 적용되지 않은 예시 추가

--- cra-test/src/App.js
+++ cra-test/src/App.js
@@ -1,11 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
 import TodoList from "./TodoList";
+import Button from "./Button1";
+import Box from "./Box1";
 
 function App() {
   return (
     <div className="App">
-      <TodoList />
+      {/* <TodoList /> */}
+      <Button size="big" />
+      <Button size="small" />
+      <Box size="big" />
+      <Box size="small" />
     </div>
   );
 }
